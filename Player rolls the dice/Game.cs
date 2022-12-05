af8fbc6fa641f33a92d1d60c44b44c0fc7948cc7
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-05 20:14:14 +0530

    Player rolls the dice

---
+++ Player rolls the dice/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Player_rolls_the_dice
+{
+    internal class Game
+    {
+       static int player1, position = 0;
+       static Random random= new Random();
+       public static void Turn()
+        { 
+         int player1=random.Next(1,7);
+            Console.WriteLine(player1);
+        }
+
+    }
+}
---
+++ Player rolls the dice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Player_rolls_the_dice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.Turn();
+        }
+    }
+}
