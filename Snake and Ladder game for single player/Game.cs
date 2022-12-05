37acfa885643d4a2a328b0fb40a59d5d25773996
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-05 20:08:19 +0530

    Game for single player

---
+++ Snake and Ladder game for single player/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake_and_Ladder_game_for_single_player
+{
+    internal class Game
+    {
+        int player1, position = 0;
+    }
+}
---
+++ Snake and Ladder game for single player/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_and_Ladder_game_for_single_player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder game");
+        }
+    }
+}
